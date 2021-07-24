3bab6c479daa0da064c284fb98c74c7f169e10a8
hanifnml <hanif.nmls@gmail.com>
2021-07-24 21:10:31 +0700

    editing some files

---
+++ src/handler.js
@@ -0,0 +1,65 @@
+const {filter} = require('lodash');
+const {nanoid} = require('nanoid');
+const bookshelf = require('./bookshelf');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = (pageCount === readPage) ? true : false;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  bookshelf.push(newBook);
+
+  const isSuccess = bookshelf.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    }).code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'error',
+    message: 'Buku gagal ditambahkan',
+  }).code(500);
+  return response;
+};
+
+const getAllBooksHandler = (request, h) => {
+  status: 'success';
+  data: {
+    bookshelf;
+  }
+};
+
+module.exports = {addBookHandler, getAllBooksHandler};
